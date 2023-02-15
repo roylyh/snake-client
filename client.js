453bbea332eb084a14a589b773cfa4c761fdd800
roylyh <roy.louyihui@gmail.com>
2023-02-15 15:42:02 -0800

    Snake Initials

--- client.js
+++ client.js
@@ -10,6 +10,14 @@ const connect = function() {
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
+  // event "connect" handler
+  conn.on("connect",() => {
+    console.log("Successfully connected to game server");
+    conn.write("Name: ROY");
+  }
+  );
+
+  // event data handler
   conn.on("data",(data) => {
     console.log("data:",data);
   }
